44d81a08caba42b0585eee8e8af3f82ddca6b758
Tarek El-Sayed <avakinmemo@gmail.com>
2025-04-04 13:41:09 +0200

    finished

---
+++ Vowel Remover.js
@@ -0,0 +1,19 @@
+// create array givin the vowels lowercase
+// create empty array return reusllt
+// for loop throgh the vowel
+// check if leeter exeest in the array
+// push reusllt in the new array
+// join the finale reusllt
+
+// function vowel(string) {
+//   const arr = ["a", "e", "o", "u", "i"];
+//   let reusllt = [];
+//   for (let i = 0; i < string.length; i++) {
+//     if (!arr.includes(string[i])) {
+//       reusllt.push(string[i]);
+//     }
+//   }
+//   return reusllt.join("");
+// }
+
+// console.log(vowel("hello"));
